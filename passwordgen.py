707a02d75dbaedec680286ef4320fbdda67ce619
Owen Mather <owmatha@gmail.com>
2021-08-27 14:59:28 +0800

    12/08 First draft rockpaperscissors.py 27/08 Start work on passwordgen.py

--- passwordgen.py
+++ passwordgen.py
@@ -1,17 +1,37 @@
-from random import randint
-alphaLower = 'abcdefghijklmnopqrstyvwxyz'
-alphaUpper = 'ABCDEFGHIJKLMNOPQRSTYVWXYZ'
-symbols = '(^:?+){/!&$;#,@'
+from random import randint, shuffle
 
-size = input("How long do you want your password?")
-symbolCheck = input(f"Do you want to have symbols? ie: {symbols}")
+symbols = '(^-:?+){/!&$;#,@0123456789'
+
+size = int(input("How long do you want your password?"))
+#symbolCheck = input(f"Do you want to have symbols? ie: {symbols}")
 
 #Time to generate the password
 
 #Elements that will make the password
-tempPass = ''
+tempPass = []
 
 #Select some lowers
+ ##ASCII lowers 97-122
+ ##ASCII uppers 65-90
 
-for i in range(randint(a, size-(int(size/3)))):
-    print('i')
+#amount of each
+symbolsNo = int(size/3)
+lowersNo = size-(symbolsNo)
+uppersNo = size-lowersNo
+
+for i in range(lowersNo):
+    tempPass.append(chr(randint(97,122)))
+
+for i in range(uppersNo):
+    tempPass.append(chr(randint(65,90)))
+
+for i in range(symbolsNo):
+    tempPass.append(symbols[randint(0, len(symbols))])
+
+shuffle(tempPass)
+
+print(f'Your password is: {"".join(tempPass)}')
+
+
+
+
